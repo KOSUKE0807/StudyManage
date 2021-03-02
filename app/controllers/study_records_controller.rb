80bbce865003e703b9fc7704951ce7548bd59e4c
Kosuke Fujita <kousuke@ezweb.ne.jp>
2021-03-02 21:46:27 +0900

    import kaminari and create pagenation

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -5,5 +5,10 @@ class StudyRecordsController < ApplicationController
   end
 
   def index
+    records = StudyRecord.where(student_id: @current_user.id)
+    records = records.page(params[:page]).per(5)
+    @records = records.all.order(id: "DESC")
+    @r = []
+    @records.each{|r| @r.push(r)}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ StudyRecord.create({
   comment: "今日は三次関数、複素数平面を勉強しました。",
   subject: "数学",
   student_id: 1,
-  created_at: '2021/01/01 12:00:00'
+  created_at: '2021/01/03 12:00:00'
 })
 StudyRecord.create({
   study_time: "02:30:00",
@@ -19,7 +19,7 @@ StudyRecord.create({
   comment: "今日は空間ベクトル、正射影ベクトルを勉強しました。",
   subject: "数学",
   student_id: 1,
-  created_at: '2021/01/02 12:00:00'
+  created_at: '2021/01/04 12:00:00'
 })
 StudyRecord.create({
   study_time: "01:00:00",
@@ -27,5 +27,29 @@ StudyRecord.create({
   comment: "今日は前置詞、イディオムを勉強しました。",
   subject: "英語",
   student_id: 1,
-  created_at: '2021/01/03 12:00:00'
+  created_at: '2021/01/05 12:00:00'
+})
+StudyRecord.create({
+  study_time: "01:30:00",
+  study_text: "青チャート",
+  comment: "今日は三次関数、複素数平面を勉強しました。",
+  subject: "数学",
+  student_id: 1,
+  created_at: '2021/01/06 12:00:00'
+})
+StudyRecord.create({
+  study_time: "02:30:00",
+  study_text: "文系数学のプラチカ",
+  comment: "今日は空間ベクトル、正射影ベクトルを勉強しました。",
+  subject: "数学",
+  student_id: 1,
+  created_at: '2021/01/07 12:00:00'
+})
+StudyRecord.create({
+  study_time: "01:00:00",
+  study_text: "Vintage",
+  comment: "今日は前置詞、イディオムを勉強しました。",
+  subject: "英語",
+  student_id: 1,
+  created_at: '2021/01/08 12:00:00'
 })
