f6bb52e9ba2328c7bcb5d5fc089b322456aae18c
Kosuke Fujita <kousuke@ezweb.ne.jp>
2021-03-02 15:41:21 +0900

    create study_record data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+StudyRecord.create({
+  study_time: "01:30:00",
+  study_text: "青チャート",
+  comment: "今日は三次関数、複素数平面を勉強しました。",
+  subject: "数学",
+  student_id: 1,
+  created_at: '2021/01/01 12:00:00'
+})
+StudyRecord.create({
+  study_time: "02:30:00",
+  study_text: "文系数学のプラチカ",
+  comment: "今日は空間ベクトル、正射影ベクトルを勉強しました。",
+  subject: "数学",
+  student_id: 1,
+  created_at: '2021/01/02 12:00:00'
+})
+StudyRecord.create({
+  study_time: "01:00:00",
+  study_text: "Vintage",
+  comment: "今日は前置詞、イディオムを勉強しました。",
+  subject: "英語",
+  student_id: 1,
+  created_at: '2021/01/03 12:00:00'
+})
